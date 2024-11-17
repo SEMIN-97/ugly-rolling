ac292b894e366a9d10c93893c06bf67b2a279a4e
SEMIN-97 <jsemin.1021@gmail.com>
2024-11-17 14:19:07 +0900

    Chore: React Query 설치 및 lint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pluginQuery from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +27,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...pluginQuery.configs['flat/recommended']
     },
   },
 )
