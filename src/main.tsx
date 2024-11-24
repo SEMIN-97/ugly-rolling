0f1dec066dd9a764de0a8eebd7e7b74b16e09084
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-11-24 13:28:23 +0900

    Feat: QueryClient 설정 파일 생성 및 main.tsx에 QueryClientProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import './index.css';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./config/queryClient.ts";
 
 const router = createRouter({ routeTree });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={ queryClient }>
+      <RouterProvider router={ router }/>
+    </QueryClientProvider>
   </StrictMode>,
 );
