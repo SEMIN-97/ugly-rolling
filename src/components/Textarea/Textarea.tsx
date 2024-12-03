4d4e37fc3e87673b3274d6980da40250ce6cf7ad
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-03 21:47:16 +0900

    feat: Textarea 컴퍼넌트 스타일 추가 및 height 프로퍼티 추가
    #5

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,17 +1,20 @@
 import { ChangeEvent, FC } from "react";
+import styles from "./Textarea.module.scss";
 
 interface TextareaProps {
   value: string;
   onChange: (value: string) => void;
   placeholder: string;
   maxLength: number;
+  height: number;
 }
 
 export const Textarea: FC<TextareaProps> = ({
                                               value,
                                               onChange,
                                               placeholder = '',
-                                              maxLength = 100
+                                              maxLength = 100,
+                                              height = 100
                                             }) => {
   const currentLength = value.length;
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,15 +22,16 @@ export const Textarea: FC<TextareaProps> = ({
   };
 
   return (
-    <div>
+    <div className={ styles.textareaContainer } style={ { height } }>
       <textarea
+        className={ styles.textarea }
         value={ value }
         onChange={ handleChange }
         placeholder={ placeholder }
         maxLength={ maxLength }
       />
       { maxLength && (
-        <div>{ currentLength }/{ maxLength }</div>
+        <div className={ styles.length }>{ currentLength }/{ maxLength }</div>
       ) }
     </div>
   );
