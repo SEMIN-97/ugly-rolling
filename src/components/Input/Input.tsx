1cc34fd2773361f62a14a61ccb79d0e552e34895
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-03 20:37:05 +0900

    feat: Input 컴퍼넌트 생성 및 InputProps 정의
    #2

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent, FC } from "react";
+
+interface InputProps {
+  placeholder: string;
+  maxLength: number;
+  value: string;
+  onChange: (value: string) => void;
+  validate: (value: string) => boolean;
+}
+
+export const Input: FC<InputProps> = ({ placeholder, maxLength, value, onChange, validate = () => true }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const targetValue = e.target.value;
+    if (validate(targetValue)) {
+      onChange(targetValue);
+    }
+  };
+
+  return (<input
+    type='text'
+    placeholder={ placeholder }
+    maxLength={ maxLength }
+    value={ value }
+    onChange={ handleChange }
+  ></input>);
+};
