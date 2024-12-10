cd46925ca531a9f12ce4054c94f471f62620e715
Semin <jsemin.1021@gmail.com>
2024-12-10 20:38:59 +0900

    style: reset.scss 추가 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 import { queryClient } from './config/queryClient.ts';
-
+import './styles/index.scss';
 
 const router = createRouter({ routeTree });
 
