4d900f65ac08b70d71c7c10794b070f2e38b5fcd
Semin <jsemin.1021@gmail.com>
2024-12-10 20:37:45 +0900

    style: import 문의 따옴표 변경 (" -> ')

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./config/queryClient.ts";
+import { queryClient } from './config/queryClient.ts';
+
 
 const router = createRouter({ routeTree });
 
@@ -12,5 +13,5 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={ queryClient }>
       <RouterProvider router={ router }/>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
