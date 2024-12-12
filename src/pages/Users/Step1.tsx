2faed4d8ebc845bd6113f88c5ae64b3f558961e1
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-12 21:07:06 +0900

    feat: Step1 컴퍼넌트 스타일 적용
    #6

--- src/pages/Users/Step1.tsx
+++ src/pages/Users/Step1.tsx
@@ -12,21 +12,26 @@ interface Step1Props {
 
 export const Step1: FC<Step1Props> = ({ nickname, setNickname, handleNextStep }) => {
   return (
-    <>
-      <Title bold>반가워요! 🎅</Title>
-      <Title bold>친구들에게 어떻게 불리나요?</Title>
-      <Input
-        className={ styles.input }
-        placeholder='닉네임을 입력해주세요'
-        value={ nickname }
-        onChange={ setNickname }
-        validate={ () => ({ maxLength: 20 }) }
-      ></Input>
-      <Button
-        label='다음'
-        onClick={ handleNextStep }
-        isDisabled={ !nickname }
-      />
-    </>
+    <div className={ styles.step1Container }>
+      <div className={ styles.step1Body }>
+        <Title bold>반가워요! 🎅</Title>
+        <Title bold>친구들에게 어떻게 불리나요?</Title>
+        <div className={ styles.inputContainer }>
+          <Input
+            placeholder='닉네임을 입력해주세요'
+            value={ nickname }
+            onChange={ setNickname }
+            validate={ () => ({ maxLength: 20 }) }
+          ></Input>
+        </div>
+      </div>
+      <div className={ styles.buttonContainer }>
+        <Button
+          label='다음'
+          onClick={ handleNextStep }
+          isDisabled={ !nickname }
+        />
+      </div>
+    </div>
   );
 };
