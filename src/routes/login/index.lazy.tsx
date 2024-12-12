464ab8f896f7704fbeffbbcc31fbb6387e265743
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-12 21:38:25 +0900

    feat: login 컴퍼넌트 스타일 적용
    #6

--- src/routes/login/index.lazy.tsx
+++ src/routes/login/index.lazy.tsx
@@ -2,6 +2,8 @@ import { createLazyFileRoute } from '@tanstack/react-router';
 import kakaoLogin from '../../assets/login/kakao-login.png';
 import { useKakaoInit, useKakaoLogin } from "../../hooks/login";
 import { SubTitle, Title } from "../../components/Title/Title.tsx";
+import { CommonLayout } from "../../layouts/CommonLayout.tsx";
+import styles from './index.lazy.module.scss';
 
 export const Route = createLazyFileRoute('/login/')({
   component: Login,
@@ -12,13 +14,17 @@ function Login() {
   const { handleKakaoLogin } = useKakaoLogin();
 
   return (
-    <>
-      <Title bold>크리스마스 스웨터 롤링페이퍼</Title>
-      <SubTitle>귀여운 스웨터를 통해 메세지를 주고받아요.</SubTitle>
-      <button onClick={ handleKakaoLogin }>
-        <img src={ kakaoLogin }/>
-      </button>
-    </>
+    <CommonLayout>
+      <div className={ styles.loginContainer }>
+        <Title bold>크리스마스 스웨터 롤링페이퍼</Title>
+        <SubTitle>귀여운 스웨터를 통해 메세지를 주고받아요.</SubTitle>
+        <div className={ styles.buttonContainer }>
+          <button onClick={ handleKakaoLogin }>
+            <img src={ kakaoLogin }/>
+          </button>
+        </div>
+      </div>
+    </CommonLayout>
   );
 }
 
