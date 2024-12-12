e4af69b5cd779910837b4d539887fc709c21e513
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-12 18:31:08 +0900

    feat: login 페이지 추가 내용 작성
    #6

--- src/routes/login/index.lazy.tsx
+++ src/routes/login/index.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import kakaoLogin from '../../assets/login/kakao-login.png';
 import { useKakaoInit, useKakaoLogin } from "../../hooks/login";
+import { SubTitle, Title } from "../../components/Title/Title.tsx";
 
 export const Route = createLazyFileRoute('/login/')({
   component: Login,
@@ -11,9 +12,13 @@ function Login() {
   const { handleKakaoLogin } = useKakaoLogin();
 
   return (
-    <button onClick={ handleKakaoLogin }>
-      <img src={ kakaoLogin }/>
-    </button>
+    <>
+      <Title bold>크리스마스 스웨터 롤링페이퍼</Title>
+      <SubTitle>귀여운 스웨터를 통해 메세지를 주고받아요.</SubTitle>
+      <button onClick={ handleKakaoLogin }>
+        <img src={ kakaoLogin }/>
+      </button>
+    </>
   );
 }
 
