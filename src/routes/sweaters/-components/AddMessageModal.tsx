a37fe7b0bda1556af9b36029119b0cd2edadd670
SEMIN-97 <jsemin.1021@gmail.com>
2024-12-15 23:02:16 +0900

    feat: SelectOrnament 컴퍼넌트 기본 기능 구현

--- src/routes/sweaters/-components/AddMessageModal.tsx
+++ src/routes/sweaters/-components/AddMessageModal.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Modal } from '../../../components/modal/modal.tsx';
 import { Button } from '../../../components/Button/Button.tsx';
+import { OrnamentType } from '../../../types/enum';
+import { ornamentList } from '../../../models/ornamentList.ts';
 import { useMessageStore } from '../-stores/-messageStore.ts';
 import { SelectOrnament } from './SelectOrnament.tsx';
 import { InputMessage } from './InputMessage.tsx';
@@ -13,15 +15,24 @@ export const AddMessageModal: FC = ({
   onClose
 }: AddOrnamentModalProps) => {
   const { ornament } = useMessageStore();
+  const [selectedOrnament, setSelectedOrnament] = useState<OrnamentType>(ornamentList[0]);
+
   const onClick = () => {
     console.log('');
   };
 
+  const bodyContent = ornament ? (
+    <InputMessage />
+  ) : (
+    <SelectOrnament
+      selectedOrnament={selectedOrnament}
+      setSelectedOrnament={setSelectedOrnament}
+    />
+  );
+
   return (
     <Modal
-      body={
-        ornament ? <InputMessage /> : <SelectOrnament />
-      }
+      body={bodyContent}
       footer={
         <Button
           label={ornament ? '작성 완료' : '다음'}
--- src/routes/sweaters/-components/SelectOrnament.tsx
+++ src/routes/sweaters/-components/SelectOrnament.tsx
@@ -1,7 +1,46 @@
 import { FC } from 'react';
+import { OrnamentType } from '../../../types/enum';
+import { ornamentList } from '../../../models/ornamentList.ts';
+import styles from './AddMessageModal.module.scss';
 
-export const SelectOrnament: FC = () => {
+interface SelectOrnamentProps {
+  selectedOrnament: OrnamentType;
+  setSelectedOrnament: (ornament: OrnamentType) => void;
+}
+
+interface OrnamentItemProps {
+  item: OrnamentType;
+}
+
+export const SelectOrnament: FC<SelectOrnamentProps> = ({
+  selectedOrnament,
+  setSelectedOrnament
+}) => {
+  const handleOrnamentClick = (ornament: OrnamentType) => {
+    setSelectedOrnament(ornament);
+  };
+
+  const OrnamentItem: FC<OrnamentItemProps> = ({item}) => (
+    <li className={selectedOrnament === item ? styles.selected : ''}>
+      <button
+        type="button"
+        onClick={() => handleOrnamentClick(item)}
+      >
+        {item}
+      </button>
+    </li>
+  );
+
   return (
-    <p>SelectOrnament</p>
+    <>
+      <p>장식을 골라주세요.</p>
+      <ul className={styles.ornamentList}>
+        {
+          ornamentList.map(item => (
+            <OrnamentItem key={item} item={item}/>
+          ))
+        }
+      </ul>
+    </>
   );
 };
