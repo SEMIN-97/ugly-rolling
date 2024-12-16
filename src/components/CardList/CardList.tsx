e38c490685c4f01961320bd898e67133cd007c1f
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-16 14:42:12 +0900

    fix: CardList Component 구조 변경

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,26 +1,32 @@
 import styles from './CardList.module.scss';
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface CardListProp {
-  count: number;
-  activeIndex: number;
+  imagePath: string;
+  cards: string[];
   height: number;
-  onClick: (index: number) => void
+  onClick: (type: any) => void
 }
 
-export const CardList: FC<CardListProp> = ({ count, activeIndex, height, onClick }) => {
-  const cards = Array.from({ length: count });
+export const CardList: FC<CardListProp> = ({ imagePath, cards, height, onClick }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const imagePathPrefix = 'src/assets/images/';
+
+  const handleClick = (index: number, type: string) => {
+    setActiveIndex(index);
+    if (onClick) onClick(type);
+  };
 
   return (
     <div className={ styles.cardList }>
-      { cards.map((_, index) => (
+      { cards.map((type, index) => (
         <div
+          key={ index }
           style={ { height } }
-          key={ index + 1 }
-          className={ `${ styles.card } ${ activeIndex === (index + 1) ? styles.active : '' }` }
-          onClick={ () => onClick(index + 1) }
+          className={ `${ styles.card } ${ activeIndex === index ? styles.active : '' }` }
+          onClick={ () => handleClick(index, type) }
         >
-          Card { index + 1 }
+          <img src={ `${ imagePathPrefix }${ imagePath }${ type }.png` } className={ styles.image }/>
         </div>
       )) }
     </div>
