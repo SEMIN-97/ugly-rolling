55b49d0c56733d8a2d6f2d8b3856e933abea4afd
Semin <jsemin.1021@gmail.com>
2024-12-17 00:09:20 +0900

    feat: 메시지의 최소 글자수를 만족하지 않으면 토스트 메시지를 노출시킨다

--- src/routes/sweaters/-components/AddMessageModal.tsx
+++ src/routes/sweaters/-components/AddMessageModal.tsx
@@ -1,8 +1,9 @@
 import { FC, useState } from 'react';
+import { OrnamentType } from '../../../types/enum';
+import { useToastStore } from '../../../stores/toastStore.ts';
+import { ornamentList } from '../../../models/ornamentList.ts';
 import { Modal } from '../../../components/modal/modal.tsx';
 import { Button } from '../../../components/Button/Button.tsx';
-import { OrnamentType } from '../../../types/enum';
-import { ornamentList } from '../../../models/ornamentList.ts';
 import { useMessageStore } from '../-stores/-messageStore.ts';
 import { SelectOrnamentStep } from './SelectOrnamentStep.tsx';
 import { InputMessageStep } from './InputMessageStep.tsx';
@@ -15,6 +16,7 @@ export const AddMessageModal: FC<AddOrnamentModalProps> = ({
   onClose
 }: AddOrnamentModalProps) => {
   const { ornament, setOrnament, setMessage, receiver } = useMessageStore();
+  const addToast = useToastStore(state => state.addToast);
   const [selectedOrnament, setSelectedOrnament] = useState<OrnamentType>(ornamentList[0]);
   const [messageInput, setMessageInput] = useState<string>('');
 
@@ -23,6 +25,11 @@ export const AddMessageModal: FC<AddOrnamentModalProps> = ({
   };
 
   const handleSubmit = () => {
+    if (messageInput?.length < 5) {
+      addToast({ message: '최소 5자 이상 작성해 주세요.' });
+      return;
+    }
+
     setMessage(messageInput);
     onClose();
   };
