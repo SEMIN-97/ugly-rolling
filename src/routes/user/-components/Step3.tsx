bc1b4b2087b2794c69646817cba6c41449b64731
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-17 10:41:02 +0900

    feat: Title Component 제거 및 Typography Component 생성으로 인한 로직 수정
    #21

--- src/routes/user/-components/Step3.tsx
+++ src/routes/user/-components/Step3.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import { Textarea } from "../../../components/Textarea/Textarea.tsx";
 import { Button } from "../../../components/Button/Button.tsx";
-import { Description, SubTitle, Title } from "../../../components/Title/Title.tsx";
 import styles from './Steps.module.scss';
+import { Typography } from "../../../components/Typography/Typography.tsx";
 
 interface Step3Props {
   description: string;
@@ -14,11 +14,11 @@ export const Step3: FC<Step3Props> = ({ description, setDescription, handleNextS
   return (
     <div className={ styles.stepContainer }>
       <div className={ styles.stepBody }>
-        <Title bold>나의 친구들에게 보여지는</Title>
-        <Title>한마디를 남겨주세요.</Title>
+        <Typography as='h1' className='h1' bold>나의 친구들에게 보여지는</Typography>
+        <Typography as='h1' className='h1'>한마디를 남겨주세요.</Typography>
         <div className={ styles.subTitleContainer }>
-          <SubTitle bold>나의 한마디</SubTitle>
-          <Description>자유로운 한마디를 적고 친구들의 응원을 받아봐요.</Description>
+          <Typography as='h2' className='h2' bold>나의 한마디</Typography>
+          <p>자유로운 한마디를 적고 친구들의 응원을 받아봐요.</p>
         </div>
         <Textarea
           placeholder='다짐이나 새해 소망 등 친구들에게 보여지는 메시를 입력해주세요'
