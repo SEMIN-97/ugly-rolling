ffe36ff00bed4f756a613d71cfa330f63161acd6
Semin <jsemin.1021@gmail.com>
2024-12-17 16:16:25 +0900

    feat: 페이지 진입 시 유저 정보 조회 및 화면에 바인딩

--- src/routes/sweaters/$id.lazy.tsx
+++ src/routes/sweaters/$id.lazy.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { useFetchUserById } from '../../hooks/useUsers.ts';
 import { CommonLayout } from '../../layouts/CommonLayout.tsx';
 import { Button } from '../../components/Button/Button.tsx';
 import { AddMessageModal } from './-components/AddMessageModal.tsx';
@@ -11,13 +12,23 @@ export const Route = createLazyFileRoute('/sweaters/$id')({
 });
 
 function RouteComponent() {
+  const { id } = Route.useParams();
+  const { data, isLoading, error } = useFetchUserById(Number(id));
   const { setMessage, setOrnament, setReceiver } = useMessageStore();
   const [isShowAddModal, setIsShowAddModal] = useState(false);
 
+  if (isLoading) {
+    return <div>isLoading</div>; 
+  }
+
+  if (error || !data) {
+    return <div>404 Error</div>;
+  }
+
+  const nickname = data.nickname || '알 수 없음';
   const handleModalOpen = () => {
     setIsShowAddModal(true);
-    // TODO: 전역 store에 있는 닉네임을 전달해야함
-    setReceiver('닉네임');
+    setReceiver(nickname);
   };
 
   const handleModalClose = () => {
@@ -35,14 +46,14 @@ function RouteComponent() {
       <CommonLayout>
         <div className={styles.pageContainer}>
           <div className={styles.titleContainer}>
-            <h1>닉네임님의 스웨터</h1>
+            <h1>{ nickname }님의 스웨터</h1>
             <p className={styles.description}>
               <span className={styles.badge}>WISH</span>
-              <span>닉네임님의 한마디</span>
+              <span>{ nickname }님의 한마디</span>
             </p>
           </div>
           <div className={styles.sweaterContainer}>
-
+            <img src={`/src/assets/images/sweaters/${data.sweater_type}.png`} alt=""/>
           </div>
           <Button label="롤링페이퍼 남기기" onClick={handleModalOpen}/>
         </div>
