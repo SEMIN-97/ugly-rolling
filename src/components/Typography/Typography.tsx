078be73d8622a6947e70775dd22d06f8578830aa
hyeonbang <hyeonkyeong.bang815@gmail.com>
2024-12-17 10:39:42 +0900

    feat: Typography Component 생성
    #21

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,17 @@
+import { ElementType, FC, ReactNode } from "react";
+import styles from './Typography.module.scss';
+
+interface TypographyProps {
+  children: ReactNode;
+  as?: ElementType;
+  className?: string;
+  bold?: boolean;
+}
+
+export const Typography: FC<TypographyProps> = ({ as: Tag = 'p', bold = false, children, className = '' }) => {
+  return (
+    <Tag className={ `${ styles[className] } ${ bold ? styles.bold : '' }` }>
+      { children }
+    </Tag>
+  );
+};
