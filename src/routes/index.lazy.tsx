13750236aa4b0ebb8fe7d809a6d85c95b4623319
Semin <jsemin.1021@gmail.com>
2024-12-23 14:35:26 +0900

    splash 시간 조정

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,12 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { CommonLayout } from '../layouts/CommonLayout.tsx';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Route = createLazyFileRoute('/')({
   component: Index
 });
 
 function Index() {
-  const videoRef = useRef(null);
   const [isShowSplashVideo, setIsShowSplashVideo] = useState<boolean>(true);
   const [isFadingOut, setIsFadingOut] = useState<boolean>(false);
   const [isVideoLoaded, setIsVideoLoaded] = useState<boolean>(false); // 영상 로드 상태
@@ -22,11 +21,11 @@ function Index() {
     if (isVideoLoaded) {
       const timer = setTimeout(() => {
         setIsFadingOut(true); // 페이드 아웃 시작
-      }, 1000); // 1.3초 후 페이드 아웃 시작
+      }, 1700); // 1.3초 후 페이드 아웃 시작
 
       const fadeOutTimer = setTimeout(() => {
         setIsShowSplashVideo(false); // 비디오 제거
-      }, 5300); // 페이드 아웃 후 1초 뒤에 비디오를 DOM에서 제거
+      }, 2000); // 페이드 아웃 후 1초 뒤에 비디오를 DOM에서 제거
 
       // 타이머 정리
       return () => {
